7394c81fac26dba3184a8e6a422fca0d352d8c6d
foreman23 <foremanb@oregonstate.edu>
2023-05-30 21:19:53 -0700

    debug

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,13 @@ import { React, useState, useEffect } from 'react'
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
 import { BlogList } from "../components/BlogList";
+import { Loader } from "semantic-ui-react";
 import BlogCard from "../components/BlogCard";
 
 const Home = () => {
 
     const [blogData, setBlogData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const API_KEY = process.env.REACT_APP_API_KEY;
     const TLD = process.env.REACT_APP_TLD;
@@ -21,6 +23,7 @@ const Home = () => {
                     },
                 });
                 setBlogData(response.data);
+                setLoading(false);
             } catch (error) {
                 console.log(error);
             }
@@ -30,21 +33,32 @@ const Home = () => {
 
     return (
         <div>
+
+            {loading ? (
+                <div>
+                    <Loader active>
+                    </Loader>
+                </div>
+            ) : (
+            <div>
             <Container style={{ marginTop: '50px' }} className="homeContainerMain">
                 <Row>
                     <Col>
                         <h2 style={{ marginLeft: '10px' }}>Latest Posts</h2>
-                        {/* <div className='cardContainer'>
+                        <div className='cardContainer'>
                             {blogData.slice(0, 4).map((blog) => (
                                 <BlogCard key={blog._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blog.img}`} keyID={blog._id} title={blog.title} date={blog.date} description={blog.description}></BlogCard>
                             ))}
-                        </div> */}
+                        </div>
                     </Col>
                     <Col>
                         <BlogList viewall={true} amount={8}></BlogList>
                     </Col>
                 </Row>
             </Container>
+        </div>
+
+        )}
         </div>
     )
 }
