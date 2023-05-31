7253e3ebe131f6b8c1c3ab56af852a5455507441
foreman23 <foremanb@oregonstate.edu>
2023-05-30 22:10:17 -0700

    change back to .slice

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -15,7 +15,6 @@ const Home = () => {
     const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME;
 
     useEffect(() => {
-        console.log('asjkldklsajdklsajdl')
         const fetchData = async () => {
             try {
                 const response = await axios.get(`${TLD}blogs`, {
@@ -23,11 +22,9 @@ const Home = () => {
                         'X-API-Key': API_KEY,
                     },
                 });
-                console.log(12314)
                 setBlogData(response.data);
                 setLoading(false);
             } catch (error) {
-                console.log(812392173)
                 console.log(error);
             }
         };
@@ -49,20 +46,9 @@ const Home = () => {
                             <Col>
                                 <h2 style={{ marginLeft: '10px' }}>Latest Posts</h2>
                                 <div className='cardContainer'>
-                                    <div>
-                                        {blogData.length > 0 && (
-                                            <BlogCard key={blogData[0]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[0].img}`} keyID={blogData[0]._id} title={blogData[0].title} date={blogData[0].date} description={blogData[0].description}></BlogCard>
-                                        )}
-                                        {blogData.length > 1 && (
-                                            <BlogCard key={blogData[1]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[1].img}`} keyID={blogData[1]._id} title={blogData[1].title} date={blogData[1].date} description={blogData[1].description}></BlogCard>
-                                        )}
-                                        {blogData.length > 2 && (
-                                            <BlogCard key={blogData[2]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[2].img}`} keyID={blogData[2]._id} title={blogData[2].title} date={blogData[2].date} description={blogData[2].description}></BlogCard>
-                                        )}
-                                        {blogData.length > 3 && (
-                                            <BlogCard key={blogData[3]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[3].img}`} keyID={blogData[3]._id} title={blogData[3].title} date={blogData[3].date} description={blogData[3].description}></BlogCard>
-                                        )}
-                                    </div>
+                                    {blogData.slice(0, 4).map((blog) => (
+                                        <BlogCard key={blog._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blog.img}`} keyID={blog._id} title={blog.title} date={blog.date} description={blog.description}></BlogCard>
+                                    ))}
                                 </div>
                             </Col>
                             <Col>
