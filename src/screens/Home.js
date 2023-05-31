e79a575a321d1cdfb5665418a3d64e99dff3a2b3
foreman23 <foremanb@oregonstate.edu>
2023-05-30 21:38:07 -0700

    changed from using map and slice

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -40,25 +40,36 @@ const Home = () => {
                     </Loader>
                 </div>
             ) : (
-            <div>
-            <Container style={{ marginTop: '50px' }} className="homeContainerMain">
-                <Row>
-                    <Col>
-                        <h2 style={{ marginLeft: '10px' }}>Latest Posts</h2>
-                        <div className='cardContainer'>
-                            {blogData !== null && blogData.slice(0, 4).map((blog) =>  (
-                                <BlogCard key={blog._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blog.img}`} keyID={blog._id} title={blog.title} date={blog.date} description={blog.description}></BlogCard>
-                            ))}
-                        </div>
-                    </Col>
-                    <Col>
-                        <BlogList viewall={true} amount={8}></BlogList>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                <div>
+                    <Container style={{ marginTop: '50px' }} className="homeContainerMain">
+                        <Row>
+                            <Col>
+                                <h2 style={{ marginLeft: '10px' }}>Latest Posts</h2>
+                                <div className='cardContainer'>
+                                    <div>
+                                        {blogData.length > 0 && (
+                                            <BlogCard key={blogData[0]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[0].img}`} keyID={blogData[0]._id} title={blogData[0].title} date={blogData[0].date} description={blogData[0].description}></BlogCard>
+                                        )}
+                                        {blogData.length > 1 && (
+                                            <BlogCard key={blogData[1]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[1].img}`} keyID={blogData[1]._id} title={blogData[1].title} date={blogData[1].date} description={blogData[1].description}></BlogCard>
+                                        )}
+                                        {blogData.length > 2 && (
+                                            <BlogCard key={blogData[2]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[2].img}`} keyID={blogData[2]._id} title={blogData[2].title} date={blogData[2].date} description={blogData[2].description}></BlogCard>
+                                        )}
+                                        {blogData.length > 3 && (
+                                            <BlogCard key={blogData[3]._id} image={`https://res.cloudinary.com/${CLOUD_NAME}/image/upload/v1685425609/${blogData[3].img}`} keyID={blogData[3]._id} title={blogData[3].title} date={blogData[3].date} description={blogData[3].description}></BlogCard>
+                                        )}
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col>
+                                <BlogList viewall={true} amount={8}></BlogList>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
 
-        )}
+            )}
         </div>
     )
 }
